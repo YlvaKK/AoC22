30e3d1d48ee6208c167ef9d69f3fd281c98a42aa
Ylva Karlberg <ylvakarlberg@gmail.com>
2022-12-02 12:04:52 +0100

    couldn't leave good enough alone

--- 2/day_two.py
+++ 2/day_two.py
@@ -1,11 +1,3 @@
-def match_result(opponent, you):
-    if draw[opponent] == you:
-        return 3
-    elif wins_over[opponent] == you:
-        return 6
-    else:
-        return 0
-
 draw = { "A": "X", "B": "Y", "C": "Z" }
 wins_over = { "C": "X", "A": "Y", "B": "Z" }
 loses_to = { "B": "X", "C": "Y", "A": "Z" }
@@ -13,8 +5,8 @@ loses_to = { "B": "X", "C": "Y", "A": "Z" }
 shape_points = { "X": 1, "Y": 2, "Z": 3 }
 points = 0
 
-with open('2/day_two_input') as f:
-        lines = f.readlines()
+f = open('2/day_two_input')
+lines = f.readlines()
 
 for line in lines:
     shapes = line.split(" ")
@@ -24,10 +16,11 @@ for line in lines:
         you = loses_to[opponent]
     elif outcome == "Y":
         you = draw[opponent]
+        points += 3
     else:
         you = wins_over[opponent]
+        points += 6
 
     points += shape_points[you]
-    points += match_result(opponent, you)
 
 print("your points: %s" %points)
